bae02590e9f903b76834881d8a73876206f5291d
bokoch <bokoch1995@gmail.com>
2018-11-19 23:32:22 +0200

    Add regexp Add splitCSV function that transform csv line into array

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # node_parser
 Parse csv file inro sqlite
+
+Run: ```NODE_PATH=. node server.js```
+or ```npm run serve```
--- csv/index.js
+++ csv/index.js
@@ -1,18 +1,47 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 
 let filePath = path.join(__dirname, '../resource/athlete_events.csv');
+let rl;
 
 exports.open_file = function () {
-    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
-        if (!err) {
-           parseCsv(data);
-        } else {
-            console.log(err);
+    rl = readline.createInterface({
+        input: fs.createReadStream(filePath)
+    });
+
+    let i = 0;
+    rl.on('line', function (line) {
+        // if (line.splitCSV().length !== 15) {
+        //     console.log(line);
+        // }
+        if (splitCSV(line).length !== 15) {
+            parseCSV(line);
         }
+        i++;
     });
 };
 
-parseCsv = function (data) {
-    console.log(data);
+parseCSV = function (line) {
+    const regPattern = /(?:,"|^")(""|[\w\W]*?)(?=,|"$)|(?:,(?!")|^(?!"))([^,]*?)(?=$|,)/;
+
+    console.log(line);
+
+    // console.log(line.split(regPattern).length);
+    /*const lineParams = line.split('\",\"');
+    const trimParams = lineParams.map(item => {
+        // console.log(item.replace(/\"/g, ''));
+    })*/
+};
+
+splitCSV = function(str) {
+    let matches = str.match(/(\s*"[^"]+"\s*|\s*[^,]+|,)(?=,|$)/g);
+    for (let i = 0; i < matches.length; i++) {
+        matches[i] = matches[i].trim();
+        matches[i] = matches[i].replace(/(^")/, '');
+        matches[i] = matches[i].replace(/("$)/, '');
+        if (matches[i] === ',') matches[i] = '';
+    }
+    if (str[0] === ',') matches.unshift("");
+    return matches;
 };
