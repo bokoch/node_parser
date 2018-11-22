a3a66a68d0577090e9e07569ed92fa1c2b19b813
bokoch <bokoch1995@gmail.com>
2018-11-22 16:35:30 +0200

    Remove getters Add promise to parse method

--- csv/index.js
+++ csv/index.js
@@ -30,36 +30,28 @@ exports.parseFile = () => {
         input: fs.createReadStream(filePath)
     });
 
-    let i = 0;
-    rl.on('line', (line) => {
-        if (splitCSV(line).length === 15) {
-            if (i === 0) {
-                csvObjectKeys = splitCSV(line);
-            } else if (i >= 1 /*&& i <= 10*/) {
-                // map array to object
-                const mappedCsvObject = mapCsvArrayToObject(splitCSV(line));
+    return new Promise((res, rej) => {
+        let i = 0;
+        rl.on('line', (line) => {
+            if (splitCSV(line).length === 15) {
+                if (i === 0) {
+                    csvObjectKeys = splitCSV(line);
+                } else if (i >= 1 /*&& i <= 10*/) {
+                    // map array to object
+                    const mappedCsvObject = mapCsvArrayToObject(splitCSV(line));
 
-                // put csv object item into array
-                csvObjects.push(mappedCsvObject);
+                    // put csv object item into array
+                    csvObjects.push(mappedCsvObject);
+                }
+            } else {
+                // every line must contain 15 properties
+                console.log('Not Valid line: ' + line);
             }
-        } else {
-            // every line must contain 15 properties
-            console.log('Not Valid line: ' + line);
-        }
-        i++;
-    }).on('close', () => {
-        const uniqueTeamsData = getTeamsData(csvObjects);
-        const uniqueSportsData = getSportsData(csvObjects);
-        const uniqueEventsData = getEventsData(csvObjects);
-        const uniqueAthletesData = getAthletesData(csvObjects);
-
-        console.log(uniqueAthletesData);
-    });
-};
-
-getCSVObjects = () => {
-    console.log(csvObjects);
-    return csvObjects;
+            i++;
+        }).on('close', () => {
+            return res(csvObjects);
+        });
+    })
 };
 
 /**
@@ -101,64 +93,4 @@ mapCsvArrayToObject = (csvItemArray) => {
 splitCSV = (csvLine) => {
     const regPattern = /((?<=,\s*\")([^\"]*|([^\"]*\"\"[^""]*\"\"\"\"[^\"]*)|([^\"]*\"\"[^\"]*)|([^\"]*\"\"[^""]*\"\"[^\"]*)+)(?=\"\s*,))|((?<=,)[^,\"]*(?=,))|([^,\"]+)/g;
     return csvLine.match(regPattern);
-};
-
-/**
- * Return array of teams objects
- * @param objects. Return only NOC and Team properties
- */
-getTeamsData = (objects) => {
-    // get all unique objects by NOC 
-    return distinctObjectByKey(objects, propertyNames.NOC).map((item) => {
-        return {[propertyNames.ID]: item.ID, [propertyNames.NOC]: item.NOC, [propertyNames.Team]: stringHelper.trimDashAndNumbers(item.Team) };
-    });
-};
-
-/**
- * Return array of sports objects
- * @param objects. Return only NOC and Team properties
- */
-getSportsData = (objects) => {
-    // get all unique objects by Sport property
-    return distinctObjectByKey(objects, propertyNames.Sport).map((item) => {
-        return {[propertyNames.ID]: item.ID, [propertyNames.Sport]: item.Sport};
-    });
-};
-
-/**
- * Return array of events objects
- * @param objects. Return only NOC and Team properties
- */
-getEventsData = (objects) => {
-    // get all unique objects by Event property
-    return distinctObjectByKey(objects, propertyNames.Event).map((item) => {
-        return {[propertyNames.ID]: item.ID, [propertyNames.Event]: item.Event};
-    });
-};
-
-/**
- * Return array of athletes objects
- * @param objects. Return only NOC and Team properties
- */
-getAthletesData = (objects) => {
-    return distinctObjectByKey(objects, propertyNames.Name).map((item) => {
-        let params = {};
-
-        if (item.Height !== 'NA') {
-            params.height = parseFloat(item.Height);
-        }
-        if (item.Weight !== 'NA') {
-            params.weight = parseFloat(item.Weight);
-        }
-
-        const paramsJSON = JSON.stringify(params);
-
-        return {
-            'ID': item.ID,
-            'full_name': stringHelper.removeCharsInBrackets(item.Name),
-            'sex': (item.Sex === 'NA') ? null : item.Sex,
-            'params': paramsJSON,
-            'team_id': '',
-        };
-    });
 };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const db = require('db');
 const csv_parser = require('csv');
 
-csv_parser.parseFile();
+csv_parser.parseFile().then(x => console.log(x));
 
 //db.connect();
 //db.close_conn();
